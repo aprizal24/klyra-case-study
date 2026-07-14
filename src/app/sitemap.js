cd7d8685d411b3d09a2f3813c404755d04071bab
aprizaltt <aprizaltriansyah24.com>
2026-07-15 02:01:52 +0800

    Add robots and sitemap

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,7 +1,7 @@
 export default function sitemap() {
   return [
     {
-      url: "https://klyra-case-study.vercel.app",
+      url: "https://klyra-case-study.vercel.app/",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 1,
